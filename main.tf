a141948c9a6ca3e1fe62ff254a4efb0564aeddb6
TARAK <tarakaramtirumala@gmail.com>
2023-03-26 18:48:42 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
-resource "aws_docdb_cluster" "docdb" {
+resource "aws_docdb_cluster" "main" {
   cluster_identifier      = "${var.env}-docdb"
   engine                  =  var.engine
-  master_username         = "foo"
-  master_password         = "mustbeeightchars"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
+  master_username         = data.aws_ssm_parameter.user.value
+  master_password         = data.aws_ssm_parameter.pass
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = true
+}
+
+resource "aws_docdb_subnet_group" "main" {
+    name = "${var.env}-docdb"
+    subnet_ids = var.subnet_ids
+
+    tags = merge(
+        var.tags,
+        { Name = "${var.env}-subnet-group" }
+    )
 }
