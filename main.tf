546868d3a38cb108e4bfc07f63fce5eed0854d4e
TARAK <tarakaramtirumala@gmail.com>
2023-03-27 11:53:10 +0530

    changes

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "aws_docdb_cluster" "main" {
   master_username         = data.aws_ssm_parameter.user.value
   master_password         = data.aws_ssm_parameter.pass.value
   backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = true
   
 }
--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "env" {}
 variable "engine" {}
 variable "backup_retention_period" {}
-
+variable "preferred_backup_window" {}
 variable "skip_final_snapshot" {}
 variable "engine_version" {}
 variable "subnet_ids" {}
