32e2dad108b7a48a7d06c3f4acb5328b659bd2c4
TARAK <tarakaramtirumala@gmail.com>
2023-03-27 11:30:54 +0530

    changes

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ resource "aws_docdb_cluster" "main" {
   preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = true
   db_subnet_group_name = aws_docdb_subnet_group.main.name
-  kms_key_id = data.aws_kms_key.arn
   storage_encrypted = true
   
 }
